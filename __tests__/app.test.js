dbb729d6c6008ba62332652310a86369d4fd3f9e
Pedro <pedro.chn01@gmail.com>
2022-08-24 15:06:09 -0700

    adding user sign in model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,16 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const  UserService  = require('../lib/services/UserService');
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? fakeUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...fakeUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/me').send({ email, password });
+  return [agent, user];
+};
 
 const fakeUser = {
   firstName: 'Harrison',
@@ -23,6 +33,17 @@ describe('user routes', () => {
       email,
       firstName,
       lastName,
+    });
+  });
+
+  it('get returns the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+    expect(me.status).toBe(200);
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
   });
   afterAll(() => {
